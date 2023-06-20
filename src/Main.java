6eeb324ac07bdb37be0e2b7fbaaa98378554da1d
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-06-20 08:53:14 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ public class Main {
         String polinomio = sc.nextLine();
         String[] monomios = polinomio.split("(?=([+-]))");
         for (String mon : monomios) {
-            System.out.println(mon);
+            System.out.print(mon + ": ");
+            String[] parts = mon.split("([xX]\\^?)");
+            for (String pt : parts) {
+                System.out.print(pt + " ");
+            }
+            System.out.println();
         }
     }
 }
