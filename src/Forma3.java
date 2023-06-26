30b343fccefb700cf931734592e1d4f7c806dbcc
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-06-26 15:30:44 -0500

    main

--- src/Forma3.java
+++ src/Forma3.java
@@ -19,7 +19,7 @@ public class Forma3 {
 
     public void Disassemble(int[] data) {
         for (int i = 0; i < data.length; i += 2) {
-            Insert(data[i], data[i + 1]);
+            InsertOrder(data[i], data[i + 1]);
         }
     }
 
@@ -45,6 +45,83 @@ public class Forma3 {
                 aux = aux.getLiga();
             }
             aux.setLiga(P);
+        }
+    }
+
+    public void InsertStart(int coe, int exp) {
+        Nodo X = new Nodo(coe, exp);
+        if (this.Punta == null) {
+            this.Punta = X;
+        } else {
+            X.setLiga(this.Punta);
+            this.Punta = X;
+        }
+    }
+
+    public void InsertEnd(int coe, int exp) {
+        Nodo X = new Nodo(coe, exp);
+
+        if (this.Punta == null) {
+            this.Punta = X;
+        } else {
+            Nodo P = this.Punta;
+            while (P.getLiga() != null) {
+                P = P.getLiga(); //Avance de Liga
+            }
+            P.setLiga(X);
+        }
+
+    }
+
+    public void InsertOrder(int coe, int exp) {
+        Orderby();
+        if (this.Punta == null) {
+            InsertEnd(coe, exp);
+        } else if (this.Punta.getExp() > exp) {
+            InsertStart(coe, exp);
+        } else {
+            Nodo P = this.Punta;
+            Boolean Find = true;
+            while (P != null && Find) {
+                if (P.getExp() <= exp && P.getLiga() == null) {
+                    InsertEnd(coe, exp);
+                    Find = false;
+                } else if (P.getExp() <= exp && (P.getLiga()).getExp() >= exp) {
+                    Nodo X = new Nodo(coe, exp);
+                    X.setLiga(P.getLiga());
+                    P.setLiga(X);
+                    Find = false;
+                }
+                P = P.getLiga();
+            }
+        }
+        Orderby();
+    }
+
+    public void Orderby() {
+        if (this.Punta != null) { //Si esta vacia
+            if (this.Punta.getLiga() != null) {//Si solo tiene un Elemento
+                for (int K = 0; K < Len(); K++) {
+                    Nodo A = null, P = this.Punta, S = P.getLiga();
+                    while (S != null) {
+                        if (P.getExp() < S.getExp()) {
+                            if (P == Punta) {
+                                Punta = S;
+                            } else {
+                                A.setLiga(S);
+                            }
+                            P.setLiga(S.getLiga());
+                            S.setLiga(P);
+                            A = S;
+                            S = P.getLiga();
+                        } else {
+                            A = P;
+                            P = S;
+                            S = S.getLiga();
+                        }
+                    }
+                }
+            }
         }
     }
 
